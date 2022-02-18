21d152a95d17aa40b2332c8d6f8ceadfab643fcd
bugra <bgrglr.be@gmail.com>
2022-02-18 14:05:38 +0100

    6th commit

--- src/gitProject2022/lastgit.java
+++ src/gitProject2022/lastgit.java
@@ -4,7 +4,7 @@ public class lastgit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("18_02");
+		System.out.println("18___--___02");
 
 	}
 
