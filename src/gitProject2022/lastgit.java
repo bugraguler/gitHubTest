808e174336324286817ffa31bbb33ca13d19a86b
bugra <bgrglr.be@gmail.com>
2022-02-18 14:44:09 +0100

    ı have updated some cods

--- src/gitProject2022/lastgit.java
+++ src/gitProject2022/lastgit.java
@@ -5,6 +5,7 @@ public class lastgit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("18___--___02");
+		System.out.println("321321321321321321");
 
 	}
 
