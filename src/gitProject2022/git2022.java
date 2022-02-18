17ee3685b1544b17e79b20ce4c9d1f25862452dd
bugraguler <93531514+bugraguler@users.noreply.github.com>
2022-02-18 14:42:00 +0100

    Update git2022.java

--- src/gitProject2022/git2022.java
+++ src/gitProject2022/git2022.java
@@ -3,7 +3,7 @@ package gitProject2022;
 public class git2022 {
 
 	public static void main(String[] args) {
-		System.out.println("2331");
+		System.out.println("23");
 		System.out.println("nomiladino");
 	}
 
